ed67597b098cfec13cf35621d43b99ccec69c2d5
Cristian-Gomez <ycamigo123@gmail.com>
2025-04-19 12:05:03 -0500

    se creo la conexion con la base de datos ferreteriabd

---
+++ src/Conexion/ConexionBD.java
@@ -0,0 +1,16 @@
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexionBD {
+    public static Connection getConnection(){
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ferreteriabd","root","");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return  con;
+    }
+}
